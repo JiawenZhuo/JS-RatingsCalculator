ce44f70b497f81e0a3a03b733b207deb3df99da4
JiawenZhuo <jiawen.zhuo@uq.net.au>
2021-10-16 13:52:26 +1000

    module1

--- js/ratings.js
+++ js/ratings.js
@@ -1,9 +1,28 @@
-function collect_rating(){
+const collect_ratings=() =>{
     let ratings ={
-        count: 0,
-        sum : 0,
-        average :0
+        'count': 0,
+        'sum' : 0,
+        'average' :0
     }
     let rating = 0;
+
+    const elements = document.querySelectorAll(".rating");
+
+    elements.forEach((element) => {
+        rating = parseInt(element.id.replace("star", ""));
+        count += parseInt(element.value);
+        sum += parseInt(element.value) * rating;
+
+        if(count !==0){
+            average = sum/count;
+        }
+    });
+    return ratings;
+
 }
+
+document.addEventListener(("change"), ()=>{
+    const ratings = collect_ratings();
+    querySelector("#average").value = ratings.average.toFixed(2);
+})
 
